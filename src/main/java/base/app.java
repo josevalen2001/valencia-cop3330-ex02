b788e0e52042fad8a5094e11539f77563c85c65f
josevalen2001 <jose.juda.valencia@gmail.com>
2021-06-06 14:19:53 -0400

    Final Version

--- src/main/java/base/app.java
+++ src/main/java/base/app.java
@@ -1,6 +1,11 @@
 package base;
 import java.util.*;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Jose Valencia
+ */
+
 public class app {
 
     public static void main(String[] args) {
@@ -13,7 +18,6 @@ public class app {
 
         //Concat and output
         System.out.println(input+" has "+input.length()+" characters.");
-
 
     }
 
